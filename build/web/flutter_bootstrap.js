14f2cc94947c052bf606a3a2ab095aea24c12f4b
vrushti hindocha <vrushtihindocha@vrushtis-MacBook-Air.local>
2026-05-12 15:02:05 +0530

    Build latest web app

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3328549668" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2280414912" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
